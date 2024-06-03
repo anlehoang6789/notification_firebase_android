14554fd53a631c9227802ec3d9da0030f43ca61f
anlehoang6789 <anlehoang6789@gmail.com>
2024-06-03 13:36:55 +0700

    notification with firebase

---
+++ app/src/main/java/com/example/demo_notification_with_firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package com.example.demo_notification_with_firebase;
+
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        // Handle incoming messages here
+        super.onMessageReceived(remoteMessage);
+
+        getFirebaseMessage(remoteMessage.getNotification().getTitle(),
+                remoteMessage.getNotification().getBody());
+    }
+
+    private void getFirebaseMessage(String title, String body) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notify")
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        managerCompat.notify(102, builder.build());
+    }
+}
